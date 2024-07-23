01a991b8b8444164c6021c6caa9c5ad5c1b11aea
bobo459 <awd458@naver.com>
2024-07-23 16:13:04 +0900

    v0723

--- src/Components/20240723/CounterState.js
+++ src/Components/20240723/CounterState.js
@@ -38,7 +38,15 @@ export function CounterState(){
   useEffect(()=>{
     console.log("useEffect",count);
   }, [count]); //매개변수1은 콜백함수, 매개변수2는 상태 의존성배열
-  
+  // useEffect :1. 함수형 컴포넌트에서 side effect(부수효과)를 수행하고자 할때 사용하는 후크
+  //            2. 호출되는 시기가 중요한데 렌더링이 완료된 이후에 호추
+  //            3. 의존성배열이 비어 있으면? 초기 로딩시 1회만 호출
+  useEffect(()=>{
+    //빈 의존성배열의 의미는 컴포넌트가 처음 로드될때 딱 한번만 호출된다는 의미!
+    //각종 이벤트 콜백함수를 등록하는데 사용됨
+  },[])
+
+
   return(
   <>
   <Container>
